100174a652f7a43aba0da6d3edddfdba2743623c
AzureAD\EdgardOrellana <rene6484@gmail.com>
2019-04-15 10:00:35 -0600

    Initial commit

---
+++ FLC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FLC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
